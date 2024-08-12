76034540019ac20a901adc89ade9e75490435950
zack6yuan <zack6yuan@gmail.com>
2024-08-12 22:24:09 +0000

    same as last task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,5 +7,31 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        
+binary_tree_t *newnode; /*ptr to newly created node*/
+
+if (!parent) /*check if parent is NULL*/
+{
+return (NULL);
+}
+
+newnode = malloc(sizeof(binary_tree_t)); /*allocate for newnode*/
+
+if (!newnode) /*check if malloc worked*/
+{
+return (NULL);
+}
+
+newnode->n = value; /*set value of newnode*/
+newnode->parent = parent; /*set parent of newnode*/
+newnode->left = NULL; /*set right child of parent to NULL*/
+newnode->right = NULL; /*set left child of parent to NULL*/
+
+if (parent->right) /*if parent already has left child*/
+{
+newnode->right = parent->right; /*newnode left ptr*/
+newnode->right->parent = newnode; /*left child's parent*/
+}
+parent->right = newnode; /*parent's left ptr pts to new node*/
+
+return (newnode); /*return newnode ptr*/
 }
