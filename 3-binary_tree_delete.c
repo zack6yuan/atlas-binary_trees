a3a77d49354683a16edde46a55ad709b5ec1468a
zack6yuan <zack6yuan@gmail.com>
2024-08-12 22:31:08 +0000

    bye bye tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,5 +6,20 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-        
+        if (!tree) /*if no tree is present*/
+        {
+                return (NULL);
+        }
+        if (tree) /*if tree is not NULL*/
+        {
+                if (tree->left)
+                {
+                        binary_tree_delete(tree->left);
+                }
+                if (tree->right)
+                {
+                        binary_tree_delete(tree->right);
+                }
+                free(tree);
+        }
 }
