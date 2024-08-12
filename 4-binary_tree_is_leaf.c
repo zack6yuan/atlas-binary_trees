0a2be8c3e64a6ada71d8ea4358d430664f5571aa
zack6yuan <zack6yuan@gmail.com>
2024-08-12 22:40:43 +0000

    task 4 complete

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,5 +6,13 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        
+        if (!node)
+        {
+                return (0);
+        }
+        if (!node->left && !node->right)
+        {
+                return (1);
+        }
+        return (0);
 }
