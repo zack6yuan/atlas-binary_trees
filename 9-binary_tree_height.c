c85b69385f18e93e413f5a28aeaba8533de1174c
zack6yuan <zack6yuan@gmail.com>
2024-08-12 23:17:52 +0000

    task 9 complete

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,5 +6,24 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        
+        size_t left_subtree, right_subtree; /*stores heights*/
+
+        if (!tree) /*if tree is empty*/
+        {
+                return (0);
+        }
+
+        if (!tree->left && !tree->right) /*no children, leaf?*/
+        {
+                return (0);
+        }
+
+        left_subtree = binary_tree_height(tree->left) + 1; /*left height*/
+        right_subtree = binary_tree_height(tree->right) + 1; /*right height*/
+
+        if (left_subtree > right_subtree) /*compares*/
+        {
+                return (left_subtree); /*if left taller, return left*/
+        }
+        return (right_subtree); /*if right taller, return right*/
 }
