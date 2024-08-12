3757387d6367d8a0cacbc89ee80e9d929c6c74c2
zack6yuan <zack6yuan@gmail.com>
2024-08-12 23:06:41 +0000

    whitespace

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - goes through a binary tree 
+ * binary_tree_postorder - goes through a binary tree
  * using post-order traversal.
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a gfunction to call for each node.
