0530ac36b6dcd69bc232d21d4c4b4b7ca0f9d1e7
zack6yuan <zack6yuan@gmail.com>
2024-08-13 21:17:34 +0000

    task 13 complete

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,5 +7,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        
+        if (!tree) /*if no tree*/
+        {
+            return (0);
+        }
+        if (!tree->left && !tree->right) /*checks if leaf*/
+        {
+            return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+        } /*returns count of node with at least 1 child + 1 for current node*/
 }
