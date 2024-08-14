de2d1c2b945d072ca438d8c186ac659c79972487
zack6yuan <zack6yuan@gmail.com>
2024-08-14 07:59:04 +0000

    oops

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,19 +6,26 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-        size_t left_tree = binary_tree_height(tree->left);
-        size_t right_tree = binary_tree_height(tree->right);
-
         if (!tree)
         {
             return (0);
         }
+
+        size_t left_tree = binary_tree_height(tree->left);
+        size_t right_tree = binary_tree_height(tree->right);
+
         if (!tree->left && !tree->right)
         {
             return (1);
         }
         if (left_tree != right_tree)
+        {
+            return (0);
+        }
+        int perfect_left = binary_tree_is_perfect(tree->left);
+        int perfect_right = binary_tree_is_perfect(tree->right);
 
+        return (perfect_left && perfect_right);
 }
 /**
  * binary_tree_height - measure the height of a binary tree.
@@ -27,15 +34,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_tree;
-size_t right_tree;
-
 if (!tree) /*if tree is empty*/
 {
 return (0);
 }
-left_tree = binary_tree_height(tree->left) + 1; /*left height*/
-right_tree = binary_tree_height(tree->right) + 1; /*right height*/
+size_t left_tree = binary_tree_height(tree->left);
+size_t right_tree = binary_tree_height(tree->right);
 /*return max height of L and R subtrees + 1 for current node*/
 return (1 + (left_tree > right_tree ? left_tree : right_tree));
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_uncle - finds the sibling of a node.
+ * binary_tree_uncle - finds the uncle of a node.
  * @node: a pointer to the node to find the uncle.
  * Return: a pointer to the uncle node
  * NULL if node is NULL or node has no uncle.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        
+       if (!node || !node>parent || !node->parent->parent)
+       {
+            return (NULL);
+       }
 }
